4048d2ba6e1bd296e5998dcddc1f56208c716949
Ilya Smirnov <smirnov.il.ev@gmail.com>
2013-12-18 19:01:32 +0400

    в 11\1 изменен ввод: теперь коэффициенты и степени вводятя в произвольном порядке

--- Homework 11/Task 1/main.cpp
+++ Homework 11/Task 1/main.cpp
@@ -1,145 +1,146 @@
 #include "polynomial.h"
-#include "math.h"
 
 using namespace std;
 using namespace polynomialNS;
 
 int main()
 {
-	cout << "Welcome to the program of the polynomial" << endl;
+        cout << "Welcome to the program of the polynomial" << endl;
 
-	cout << "Enter the number and then something would happen!" << endl;
-	cout << "Enter 0 - exit" << endl;
-	cout << "Enter 1 - and check two polinomials on equality" << endl;
-	cout << "Enter 2 - and count value of polinomial in x" << endl;
-	cout << "Enter 3 - and add" << endl;
-	
-	enum userChoise { exit = 0, equality, count, addSomething };
-	
-	while (true)
-	{
-		cout << "Enter number: " << endl;
+        cout << "Enter the number and then something would happen!" << endl;
+        cout << "Enter 0 - exit" << endl;
+        cout << "Enter 1 - and check two polinomials on equality" << endl;
+        cout << "Enter 2 - and count value of polinomial in x" << endl;
+        cout << "Enter 3 - and add" << endl;
+        
+        enum userChoise { exit = 0, equality, count, addSomething };
+        
+        while (true)
+        {
+                cout << "Enter number: " << endl;
 
-		userChoise choise;
-		int temp = 0;
+                userChoise choise;
+                int temp = 0;
 
-		cin >> temp;
-		choise = (userChoise) temp;
+                cin >> temp;
+                choise = (userChoise) temp;
 
-		switch (choise)
-		{
-			case 0:
-				return 0;
-			
-			case 1:
-			{
-				cout << "let's get polynomials!" << endl;
+                switch (choise)
+                {
+                        case 0:
+                                return 0;
+                        
+                        case 1:
+                        {
+                                cout << "let's get polynomials!" << endl;
 
-				Polynomial p = createPolynomial();
-				Polynomial q = createPolynomial();
+                                Polynomial p = createPolynomial();
+                                Polynomial q = createPolynomial();
 
-				cout << "Enter max degree of the first polynomial" << endl;
-				int maxDegree = 0;
-				cin >> maxDegree;
+                                cout << "Enter coefficients and degree of element of the first polynomial. If you want to stop enter coefficient = 0" << endl;
 
-				cout << "Enter coefficients of the first polynomial in decreasing order of degree" << endl;
-				int coefficient = 0;
-				for (int i = maxDegree; i >= 0; --i)
-				{
-					cin >> coefficient;
-					addToPolynimial(p, i, coefficient);
-				}
+                                int coefficient = -1;
+								int degree = 0;
+								while (coefficient != 0)
+								{
+									cin >> coefficient;
+									cin >> degree;
+									if (coefficient != 0)
+										addToPolynimial(p, degree, coefficient);
+								}
+								
+								cout << "Enter coefficients and degree of element of the second polynomial. If you want to stop enter coefficient = 0" << endl;
 
-				cout << "Enter max degree of the second polynomial" << endl;
-				cin >> maxDegree;
+								coefficient = -1;
+                                while (coefficient != 0)
+								{
+									cin >> coefficient;
+									cin >> degree;
+									if (coefficient != 0)
+									addToPolynimial(q, degree, coefficient);
+								}
 
-				cout << "Enter coefficients of the second polynomial in decreasing order of degree" << endl;
-				for (int i = maxDegree; i >= 0; --i)
-				{
-					cin >> coefficient;
-					addToPolynimial(q, i, coefficient);
-				}
+                                if (equals(p, q))
+                                        cout << "Your polynomials are equal" << endl;
+                                else
+                                        cout << "Your polynomials are not equal" << endl;
 
-				if (equals(p, q))
-					cout << "Your polynomials are equal" << endl;
-				else
-					cout << "Your polynomials are not equal" << endl;
+                                deletePolynomial(p);
+                                deletePolynomial(q);
 
-				deletePolynomial(p);
-				deletePolynomial(q);
+                                break;
+                        }
 
-				break;
-			}
+                        case 2:
+                        {
+                                cout << "let's get polynomial!" << endl;
 
-			case 2:
-			{
-				cout << "let's get polynomial!" << endl;
+                                Polynomial p = createPolynomial();
+                                
+                                cout << "Enter coefficients and degree of element of polynomial. If you want to stop enter coefficient = 0" << endl;
 
-				Polynomial p = createPolynomial();
-				
-				cout << "Enter max degree of polynomial" << endl;
-				int maxDegree = 0;
-				cin >> maxDegree;
+								int coefficient = -1;
+								int degree = 0;
+                                while (coefficient != 0)
+								{
+									cin >> coefficient;
+									cin >> degree;
+									if (coefficient != 0)
+									addToPolynimial(p, degree, coefficient);
+								}
+                                
+                                cout << "Enter the point" << endl;
+                                int x = 0;
+                                cin >> x;
 
-				cout << "Enter coefficients of polynomial in decreasing order of degree" << endl;
-				int coefficient = 0;
-				for (int i = maxDegree; i >= 0; --i)
-				{
-					cin >> coefficient;
-					addToPolynimial(p, i, coefficient);
-				}
-				
-				cout << "Enter the point" << endl;
-				int x = 0;
-				cin >> x;
+                                cout << "Value of polynom in this point is " << value(p, x) << endl;
 
-				cout << "Value of polynom in this point is " << value(p, x) << endl;
+                                deletePolynomial(p);
 
-				deletePolynomial(p);
+                                break;
+                        }
 
-				break;
-			}
+                        case 3:
+                        {
+                                cout << "let's get polynomials!" << endl;
 
-			case 3:
-			{
-				cout << "let's get polynomials!" << endl;
+                                Polynomial p = createPolynomial();
+                                Polynomial q = createPolynomial();
+                                Polynomial r = createPolynomial();
 
-				Polynomial p = createPolynomial();
-				Polynomial q = createPolynomial();
-				Polynomial r = createPolynomial();
+                                cout << "Enter coefficients and degree of element of the first polynomial. If you want to stop enter coefficient = 0" << endl;
 
-				cout << "Enter max degree of the first polynomial" << endl;
-				int maxDegree = 0;
-				cin >> maxDegree;
+                                int coefficient = -1;
+								int degree = 0;
+								while (coefficient != 0)
+								{
+									cin >> coefficient;
+									cin >> degree;
+									if (coefficient != 0)
+									addToPolynimial(p, degree, coefficient);
+								}                                
+								
+								cout << "Enter coefficients and degree of element of the second polynomial. If you want to stop enter coefficient = 0" << endl;
 
-				cout << "Enter coefficients of the first polynomial in decreasing order of degree" << endl;
-				int coefficient = 0;
-				for (int i = maxDegree; i >= 0; --i)
-				{
-					cin >> coefficient;
-					addToPolynimial(p, i, coefficient);
-				}
+								coefficient = -1;
+                                while (coefficient != 0)
+								{
+									cin >> coefficient;
+									cin >> degree;
+									if (coefficient != 0)
+									addToPolynimial(q, degree, coefficient);
+								}
 
-				cout << "Enter max degree of the second polynomial" << endl;
-				cin >> maxDegree;
+                                add(p, q, r);
 
-				cout << "Enter coefficients of the second polynomial in decreasing order of degree" << endl;
-				for (int i = maxDegree; i >= 0; --i)
-				{
-					cin >> coefficient;
-					addToPolynimial(q, i, coefficient);
-				}
+                                showPolynomial(r);
 
-				add(p, q, r);
+                                deletePolynomial(p);
+                                deletePolynomial(q);
+                                deletePolynomial(r);
 
-				showPolynomial(r);
-
-				deletePolynomial(p);
-				deletePolynomial(q);
-				deletePolynomial(r);
-
-				break;
-			}
-		}
-	}
+                                break;
+                        }
+                }
+        }
 }
--- Homework 11/Task 1/polynomial.h
+++ Homework 11/Task 1/polynomial.h
@@ -1,30 +1,31 @@
 #pragma once
 #include "iostream"
+#include "math.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 namespace polynomialNS
 {
-	struct PolynomialElement
-	{
-		int degree;
-		int coefficient;
-		PolynomialElement *next;
-	};
+        struct PolynomialElement
+        {
+                int degree;
+                int coefficient;
+                PolynomialElement *next;
+        };
 
-	struct Polynomial
-	{
-		PolynomialElement *first;
-	};
+        struct Polynomial
+        {
+                PolynomialElement *first;
+        };
 
-	Polynomial createPolynomial();
-	void deletePolynomial(Polynomial &polynomial);
+        Polynomial createPolynomial();
+        void deletePolynomial(Polynomial &polynomial);
 
-	void addToPolynimial(Polynomial &polynomial, int degree, int coefficient);
-	void showPolynomial(Polynomial polynomial);
+        void addToPolynimial(Polynomial &polynomial, int degree, int coefficient);
+        void showPolynomial(Polynomial polynomial);
 
-	bool equals(Polynomial p, Polynomial q);
-	int value(Polynomial p, int x);
-	void add(Polynomial p, Polynomial q, Polynomial &r);
+        bool equals(Polynomial p, Polynomial q);
+        int value(Polynomial p, int x);
+        void add(Polynomial p, Polynomial q, Polynomial &r);
 }
